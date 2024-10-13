616bad870894718e6baf75193adb4a2347b4ef04
Dilawar Singh <dilawar@subcom.tech>
2024-10-13 23:15:15 +0530

    notify user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use chrono::prelude::*;
+use std::collections::HashSet;
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 use web_ical::{Calendar, Events};
 
@@ -9,8 +11,75 @@ fn main() {
 
     let args: Vec<String> = std::env::args().collect();
 
-    let icals = Calendar::new(&args[1]);
-    for event in &icals.events {
-        println!("{:?}", event.summary);
+    loop {
+        let mut is_notified = HashSet::<String>::new();
+        if let Err(e) = step(&args[1], &mut is_notified) {
+            tracing::warn!("Failed step: {e}");
+        }
+        std::thread::sleep(std::time::Duration::from_secs(60));
     }
+}
+
+fn step(url: &str, is_notified: &mut HashSet<String>) -> anyhow::Result<()> {
+    let events = get_events(url)?;
+    // iterate over upcoming events.
+    for event in events
+        .iter()
+        .filter(|a| (a.dtstart - Utc::now()).num_seconds() > 0)
+    {
+        print_event(event);
+
+        // notify user 3 minutes before that event.
+        if time_to_event_secs(event) > 3 * 60 {
+            tracing::info!(
+                "Event {} is far away in the future {:?}.",
+                event.summary,
+                event.dtstart
+            );
+            continue;
+        }
+
+        if is_notified.get(&event.summary).is_none() {
+            if notify_user(event).is_ok() {
+                is_notified.insert(event.summary.clone());
+            }
+        } else {
+            tracing::warn!("Already notified user about it.");
+        }
+    }
+    Ok(())
+}
+
+fn get_events(url: &str) -> anyhow::Result<Vec<Events>> {
+    let icals = Calendar::new(url)?;
+    let mut events = icals.events;
+    events.sort_by(|a, b| a.dtstart.cmp(&b.dtstart));
+    Ok(events)
+}
+
+/// Notify user if the event is after interval_secs
+fn notify_user(event: &Events) -> anyhow::Result<()> {
+    print_event(event);
+    notify_rust::Notification::new()
+        .summary(&format!(
+            "{} starts in {} minutes",
+            event.summary,
+            time_to_event_secs(event) / 60
+        ))
+        .timeout(notify_rust::Timeout::Never)
+        .show()?;
+    Ok(())
+}
+
+fn time_to_event_secs(event: &Events) -> i64 {
+    (event.dtstart - Utc::now()).num_seconds()
+}
+
+fn print_event(event: &Events) {
+    let now = Utc::now();
+    println!(
+        "{:?} in {} seconds.",
+        event.summary,
+        (event.dtstart - now).num_seconds()
+    );
 }
