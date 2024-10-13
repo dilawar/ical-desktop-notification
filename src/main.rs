3c078c0defbd27b758a527c99eb92c0f2d896126
Dilawar Singh <dilawar@subcom.tech>
2024-10-13 10:52:06 +0530

    using web_ical

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
-use url::Url;
+use web_ical::{Calendar, Events};
 
-pub fn main() {
+fn main() {
     tracing_subscriber::registry()
         .with(fmt::layer())
         .with(EnvFilter::from_default_env())
         .init();
 
     let args: Vec<String> = std::env::args().collect();
-    let ical_url = Url::parse(&args[1]).unwrap();
-    tracing::info!("ical url: {ical_url:?}");
 
-    let ical = reqwest::blocking::get(ical_url).unwrap().text().unwrap();
-    println!("{ical}");
+    let icals = Calendar::new(&args[1]);
+    for event in &icals.events {
+        println!("{:?}", event.summary);
+    }
 }
